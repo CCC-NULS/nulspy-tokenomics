5c50b13e12616a546e7810b81a0b54d8717e18f3
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-02-05 17:00:03 -0800

    Fixed more issues again and added more items

--- src/app_support_code.py
+++ src/app_support_code.py
@@ -49,15 +49,3 @@ class AppSupport:
 
         f.close()
 
-
-def nocache(view):
-    @wraps(view)
-    def no_cache(*args, **kwargs):
-        response = make_response(view(*args, **kwargs))
-        response.headers['Last-Modified'] = datetime.now()
-        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-        response.headers['Pragma'] = 'no-cache'
-        response.headers['Expires'] = '-1'
-        return response
-
-    return update_wrapper(no_cache, view)
