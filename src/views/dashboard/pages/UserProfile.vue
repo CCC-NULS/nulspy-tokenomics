3591bf4c8d53e00092519598eb9db85e374517a5
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-04-24 06:58:52 -0700

    changed to template

--- src/views/dashboard/pages/UserProfile.vue
+++ src/views/dashboard/pages/UserProfile.vue
@@ -9,14 +9,34 @@
         cols="12"
         md="8"
       >
-        <base-material-card>
+        <base-material-card primary>
           <template v-slot:heading>
-            <div class="display-2 font-weight-light">
-              Edit Values
+            <div class="display-2 font-weight-medium" />
+
+            <div class="display-2 font-weight-medium">
+              The following values are already set:<br><br>
+              <ul>
+                <li>
+                  Initial Supply:  &emsp;&emsp; 100,000,000 NULS<br>
+                </li>
+                <li>
+                  Inflation begins in intervals: &emsp;&emsp; 24 (2 years)<br>
+                </li>
+                <li>
+                  Inflation tokens per 12 intervals: &emsp;&emsp; 5,000,000 / 12 (416,666.66) NULS<br>
+                </li>
+                <li>
+                  Inflation is turned off at: &emsp;&emsp; 210,000,000  NULS<br>
+                </li>
+                <li>
+                  Disinflation ratio is: &emsp;&emsp; 0.004     ( 0.01 = 1% )  <br><br>
+                  Inflation and disinflation begin at the same time.<br><br>
+                </li>
+              </ul>
             </div>
 
-            <div class="subtitle-1 font-weight-light">
-              Complete your choices
+            <div class="subtitle-1b display-2 font-weight-bold">
+              &emsp;&emsp;&emsp;&emsp; <br>
             </div>
           </template>
 
@@ -27,9 +47,13 @@
                   cols="12"
                   md="4"
                 >
+                  <!-- class="inputc"  -->
                   <v-text-field
-                    label=" "
-                    disabled
+                    class="purple-input display-2"
+                    label="Initial Supply:"
+                    :value="inSupply"
+                    type="string"
+                    name="initial_supply_y"
                   />
                 </v-col>
 
@@ -37,91 +61,58 @@
                   cols="12"
                   md="4"
                 >
+                  <!-- class="inputc"  -->
                   <v-text-field
-                    class="purple-input"
-                    label="Tokens"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Tokens"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <v-text-field
-                    label="Initial Supply"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <v-text-field
-                    label="Inflation begins in intervals"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col cols="12">
-                  <v-text-field
-                    label="Inflation tokens per 12 intervals"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Inflation is turned off when inflation reaches:"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Disinflation ratio is:"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    class="purple-input"
-                    label="Inflation and disinflation begin at the same time."
+                    label="Inflation tokens per 12 intervals:"
+                    class="purple-input display-2"
+                    value="5000000"
                     type="number"
-                  />
-                </v-col>
-
-                <v-col cols="12">
-                  <v-textarea
-                    class="purple-input"
-                    label="Intervals"
-                    :value="dataoneb"
+                    name="annual_inflation"
                   />
                 </v-col>
 
                 <v-col
                   cols="12"
-                  class="text-right"
+                  md="4"
+                >
+                  <!-- class="inputc"  -->
+                  <v-text-field
+                    label="Inflation begins in how many intervals:"
+                    class="purple-input display-2"
+                    value="24"
+                    type="number"
+                    name="start_inflation"
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Inflation stops at:"
+                    class="purple-input display-2"
+                    value="210000000"
+                    type="number"
+                    name="stop_inflation_y"
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Disinflation ratio ( .01 = 1% ):"
+                    class="purple-input display-2"
+                    value="0.004"
+                    type="number"
+                    name="disinflation_ratio"
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="4"
                 >
                   <v-btn
                     color="success"
@@ -145,7 +136,7 @@
           avatar="http://westteam.nulstar.com/nms/artws/Social_Telegram_G.svg"
         >
           <v-card-text class="text-center">
-            <h6 class="display-1 mb-1 grey--text">
+            <h6 class="display-2 mb-1 grey--text">
               Tryout Values
             </h6>
 
@@ -179,10 +170,13 @@
 </template>
 
 <script>
+  const givenNumber = 100000000
+  const inSupply = givenNumber.toLocaleString('en-US')
+
   export default {
     data () {
       return {
-        // mypg: "new ptitle in CART source7777",
+        inSupply: inSupply,
         dataone: 'Try different values and view a plot of how they play out over time.',
         dataoneb: 'Intervals can be thought of as months or 30/day increments.',
         datatwo: 'For this blockchain, the following values are set: ',
