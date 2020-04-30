bb3f006efef5a9a181aeb6896eff8a1387d2bfe9
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-04-30 05:28:30 -0700

    added new file for vue.us project

--- flask_app.py
+++ flask_app.py
@@ -34,11 +34,16 @@ def index():
     return render_template('index.html')  ## has the user entry form
 
 
-@application.route('/plotfiles', methods=['GET', 'POST', 'HEAD'])
-def plots():
-    # os.chdir("..")
+@application.route('/post', methods=["GET"])   # change later
+def show_post():
+    initial_supply_y = request.args.get('a')
+    annual_inflation = request.args.get('b')
+    start_inflation = request.args.get('c')
+    stop_inflation_y = request.args.get('d')
+    disinflation_ratio = request.args.get('e')
+
     if os.name == 'nt':
-        app_root = "E:/PycharmProjects/psu-calc"
+        app_root = "E:/PycharmProjects/CCC/nulspy-tokenomics"
         # app_root = os.path.abspath(os.curdir)
     else:
         # app_root = '/home/jetgal/psucalc'  # pythonanywhere
@@ -50,12 +55,6 @@ def plots():
     plotfilesdir = os.path.join(app_root, plotsdir)
     plotfp = os.path.join(plotfilesdir, plot_name)
     plotfilepath = os.path.normpath(plotfp)
-
-    initial_supply_y = request.form['initial_supply_y']  # from index.html   # the site
-    stop_inflation_y = request.form['stop_inflation_y']  # from index.html   # the site
-    disinflation_ratio = request.form['disinflation_ratio']  # from index.html   # the site
-    annual_inflation = request.form['annual_inflation']  # from index.html   # the site
-    start_inflation = request.form['start_inflation']  # from same place
 
     args_dict = {"initial_supply_y": initial_supply_y,
                  "stop_inflation_y": stop_inflation_y,
@@ -74,6 +73,48 @@ def plots():
     with open(plotfilepath) as tfile:
         pfile_contents = tfile.read()
     return render_template_string(pfile_contents)
+
+# @application.route('/plotfiles', methods=['GET', 'POST', 'HEAD'])
+# def plots():
+#     # os.chdir("..")
+#     if os.name == 'nt':
+#         app_root = "E:/PycharmProjects/psu-calc"
+#         # app_root = os.path.abspath(os.curdir)
+#     else:
+#         # app_root = '/home/jetgal/psucalc'  # pythonanywhere
+#         app_root = '/usr/share/nginx/html/tokenlife'
+#
+#     timestp = format(datetime.now(), '%d%H%M%S')
+#     plot_name = "plot" + timestp + ".svg"
+#     plotsdir = 'plotfiles'
+#     plotfilesdir = os.path.join(app_root, plotsdir)
+#     plotfp = os.path.join(plotfilesdir, plot_name)
+#     plotfilepath = os.path.normpath(plotfp)
+#
+#     initial_supply_y = request.form['initial_supply_y']  # from index.html   # the site
+#     annual_inflation = request.form['annual_inflation']  # from index.html   # the site
+#     start_inflation = request.form['start_inflation']  # from same place
+#     stop_inflation_y = request.form['stop_inflation_y']  # from index.html   # the site
+#     disinflation_ratio = request.form['disinflation_ratio']  # from index.html   # the site
+#
+#     args_dict = {"initial_supply_y": initial_supply_y,
+#                  "stop_inflation_y": stop_inflation_y,
+#                  "disinflation_ratio": disinflation_ratio,
+#                  "annual_inflation": annual_inflation,
+#                  "start_inflation": start_inflation,
+#                  "timestp": timestp,
+#                  "plotfilepath": plotfilepath,
+#                  "plotsvg": plot_name}
+#
+#     tk_obj = appsupport.AppSupport()
+#     tk_obj.main(args_dict)
+#
+#     chk_for_file(plotfilepath)
+#
+#     with open(plotfilepath) as tfile:
+#         pfile_contents = tfile.read()
+#     return render_template_string(pfile_contents)
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
