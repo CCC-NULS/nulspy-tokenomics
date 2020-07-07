5af439d90104cd84c732d9cfd4cf91744e7c0a31
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-07-07 03:44:58 -0700

    standard commit

--- src/views/dashboard/components/AsyncCard.vue
+++ src/views/dashboard/components/AsyncCard.vue
@@ -7,42 +7,29 @@
     <v-img
       :src="bstr + incomingstr + svgstr"
     />
-
-    bstr passed in: {{ bstr }}
   </v-card>
 </template>
 
 <script>
-  const bstr = "http://westteam.nulstar.com/tokenlife/plots"
-
-  const svgstr = ".svg"
-  
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-
   export default {
     name: 'AsyncCard',
-    
     props: {
       incomingstr: {
         type: String,
-        default: 'e',        
+        default: 'empty',        
         },
     },
-
     data: () => ({
-      bstr,
-      svgstr,
+      bstr: "http://westteam.nulstar.com/tokenlife/plots",
+      svgstr: ".svg",
       dataloaded: false,
-      titlewords: 'abc',
       }),
-    
-
     async created() {
       await sleep(5000);
       this.dataloaded = true;
-
     },
   }
 </script>
