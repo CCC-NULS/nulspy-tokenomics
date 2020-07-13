e4384de36ca0055b2ede7883e8906591cf8091da
Nancy M Schorr <nms@schorrmedia.com>
2020-07-13 12:33:20 +0200

    standard commit

---
+++ runnit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+#npm run serve --port 8089 --verbose
+
+#npm help config
+
+/usr/node-v12.17.0-linux-x64/lib/node_modules/serve/bin/serve.js -s dist >/dev/null 2>&1
+
+/home/Nancy/anaconda3/bin/python flask_app.py >/dev/null 2>&1
+
+/usr/node-v12.17.0-linux-x64/lib/node_modules/serve/bin/serve.js -s dist
+
+git pull origin dev7
+
+sudo netstat -anp | grep node
+
+
+
+
+
+
