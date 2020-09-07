9e3f0454db841087b70458a6d9059ef81a6106d2
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-09-07 00:00:26 -0700

    standard commit

--- src/views/pages/Tokenomics.vue
+++ src/views/pages/Tokenomics.vue
@@ -4,19 +4,69 @@
     fluid
     tag="section"
   >
-    <v-card
-      color="red"
-      width="144px"
-      height="122px"
-    />
-    <Childc 
-      v-for="(stuf, i) in biglist"
-      slot
-      :key="i"
-      name="Childc"
-      :lead="stuf.lead"             
-      :parag="stuf.parag"             
-    />
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col 
+        cols="12"
+        md="11"
+      >
+        <v-card
+          color="grey darken-3"
+          elevation-24
+          raised
+          width="100%"
+          height="92px"
+          class="d-flex align-center justify-center"
+        >
+          <span 
+            :style="`font-family:Montserrat,sans-serif;font-size:52px;color:white;`"
+          >
+            All About Tokenomics
+          </span>
+        </v-card>
+
+        <div 
+          class="display-2 font-weight-light"
+        >
+          {{ mat1 }} 
+        </div>
+        <div 
+          class="display-2 font-weight-light"
+        >
+          {{ mat2 }} 
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-card
+        id="vcard2"
+        color="blue-grey lighten-5"
+        align="center"
+        class="grey--text text-darken-3 pr-9 pl-9 py-t pb-12 mb-12 ml-9 mr-12"
+        elevation-24
+        :style="`font-size:24px;`"
+      >
+        <v-card
+          vcard2
+          color="teal lighten-5"
+          width="70%"
+          class="d-flex align-self-center text-center pl-10 pr-24 mx-10"
+        >
+          {{ tsubtitle }}
+        </v-card>
+
+        <Childc 
+          v-for="(stuf, i) in biglist"
+          slot
+          :key="i"
+          name="Childc"
+          :lead="stuf.lead"             
+          :parag="stuf.parag"             
+        />
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
@@ -30,8 +80,8 @@
   var mat2 = 'NULS - A Fast-track business solution for developers offering a ' +
     'new industry standard in streamlining blockchain adoption.';
   
-  const tsubtitle = 'Featuring microservices, smart contracts, cross-chain interoperability, and ' +
-    'chain-building, NULS sets a new industry standard in streamlining blockchain adoption.' ;
+  const tsubtitle = 'When designing a blockchain - you may want to experiment with the ' +
+    'economics of it over time. Below is a short dictionary of the choices.'
   
   const biglist = [
       {
@@ -114,35 +164,15 @@
     },
     data () {
       return {  
-        showme: false,
-        fontpeach,
         tsubtitle,
-        // leader2,
         biglist,
-        tchipprops   
         }
       },
   };
 </script>
 <style>
-
   .montser {
     font-family: 'Montserrat', sans-serif !important;
   }
-  /* 
-  .v-chip{
-      padding-right:2px;padding-left:2px;padding-bottom:2px;padding-top:2px;
-  }
-  .v-chip__content {
-    font-size: 14px!important;
-    color: deep-purple--text;
-    padding-left: 5px;
-    padding-right: 2px;
-    font-weight: 700;
-    font-family: 'Montserrat', sans-serif !important;
-  }
 
-  .theme--light.v-chip {
-    color: purple;
-  } */
 </style>
--- src/views/pages/childc.vue
+++ src/views/pages/childc.vue
@@ -3,30 +3,44 @@
     id="Childc"
     slot
     name="Childc"
-    label
-    x-large
-    :color="color"
+    v-bind="cardprops"
     :parag="parag"
     :lead="lead"
+    class="shaped=false"
   >
-    <v-chip 
-      x-large
+    <v-chip
+      v-bind="tchipprops"
     >
       {{ lead }}
     </v-chip>
-
     {{ parag }}
   </v-card>
 </template>
 
 <script>
+  const tchipprops = {
+    color: 'grey darken-1',
+    textColor: 'white',
+    dark: true,
+    raised: true,
+    // filled: true,
+    label: true,
+    'elevation-24': true,
+    class: 'label montser mb-0 pr-2 ml-n7 mt-n1', 
+    style: "line-height:20px; padding-right:9px; box-shadow: 1px 2px 2px -1px grey",
+  }
+
+  const cardprops = {
+    color: "grey lighten-4",
+    align: "left",
+    class: "black--text pr-12 pt-6 ml-14 mr-14 mt-3 py-3",
+    elevation: 4,
+    style: "font-size:16px; line-height:24px; padding-left:70px;"
+  }
+
   export default {
     name: 'Childc',
     props: {
-      color: {
-        type: String,
-        default: "green",
-      },
       lead: {
         type: String,
         default: "Hi there lead",
@@ -34,23 +48,33 @@
       parag: {
         type: String,
         default: "Hi there twords",
-      },
-      
-    },
-    // props: {
-    //   mytext: {
-    //     type: String,
-    //     default: "howdy doody",
-    //   },
-    
+      },   
+    },   
     data: () => ({
-      tchipprops: { 
-        color: 'grey lighten-4',
-        textColor: 'black',
-        'elevation-24': true,
-        class: 'label montser mb-3 pr-1', 
-        style: "line-height:18px; box-shadow: 0px 2px 2px -1px grey",
-      }
+      tchipprops,
+      cardprops,
     })
   }
 </script>
+<style>
+
+  .montser {
+    font-family: 'Montserrat', sans-serif !important;
+  }
+    
+  .v-chip{
+      padding-right:2px;padding-left:2px;padding-bottom:2px;padding-top:2px;
+  }
+  .v-chip__content {
+    font-size: 20px!important;
+    color: white;
+    padding-left: 5px;
+    padding-right: 2px;
+    font-weight: 700;
+    font-family: 'Montserrat', sans-serif !important;
+  }
+
+  .theme--light.v-chip {
+    color: purple;
+  } 
+  </style>
