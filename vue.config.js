a8b73e1c8a5ff5ed019baba20c6054dbe6cc6b74
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-09-21 21:29:10 -0700

    standard commit

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,18 @@
 
-const westprod = false   // true in production on westteam
-
-// if (westprod) {
-//   const fs = require('fs')
-//   const https = require('https')
-//   const fn1 = "/etc/letsencrypt/archive/westteam.nulstar.com/fullchain1.pem"
-//   const fn2 = "/etc/letsencrypt/archive/westteam.nulstar.com/privkey1.pem"
-//   var devServer = {
-//     https: {
-//       cert: fs.readFileSync(fn1),
-//       key: fs.readFileSync(fn2),
-//       rejectUnauthorized: false
-//     },
-//     public: 'https://localhost:5000/',
-//   }
-// }
-// else {
-//   var devServer = ''
-// }
+const fs = require('fs')
+const https = require('https')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/tokenlife/' : '/',
   outputDir: 'dist/',
-  // devServer,
+  devServer: {
+    https: {
+      cert: fs.readFileSync("/etc/letsencrypt/archive/westteam.nulstar.com/fullchain1.pem"),
+      key: fs.readFileSync("/etc/letsencrypt/archive/westteam.nulstar.com/privkey1.pem"),
+      rejectUnauthorized: false
+    },
+  public: 'https://localhost:5000/',
+  },  
   transpileDependencies: ['vuetify'],
   pluginOptions: {
     i18n: {
