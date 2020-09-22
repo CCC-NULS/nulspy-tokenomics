1079e971e9a46a580cb5efcd7ce80585295e931c
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-09-21 21:22:40 -0700

    standard commit

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,28 @@
 
 const westprod = false   // true in production on westteam
 
-if (westprod) {
-  const fs = require('fs')
-  const https = require('https')
-  const fn1 = "/etc/letsencrypt/archive/westteam.nulstar.com/fullchain1.pem"
-  const fn2 = "/etc/letsencrypt/archive/westteam.nulstar.com/privkey1.pem"
-  var devServer = {
-    https: {
-      cert: fs.readFileSync(fn1),
-      key: fs.readFileSync(fn2),
-      rejectUnauthorized: false
-    },
-    public: 'https://localhost:5000/',
-  }
-}
-else {
-  var devServer = ''
-}
+// if (westprod) {
+//   const fs = require('fs')
+//   const https = require('https')
+//   const fn1 = "/etc/letsencrypt/archive/westteam.nulstar.com/fullchain1.pem"
+//   const fn2 = "/etc/letsencrypt/archive/westteam.nulstar.com/privkey1.pem"
+//   var devServer = {
+//     https: {
+//       cert: fs.readFileSync(fn1),
+//       key: fs.readFileSync(fn2),
+//       rejectUnauthorized: false
+//     },
+//     public: 'https://localhost:5000/',
+//   }
+// }
+// else {
+//   var devServer = ''
+// }
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/tokenlife/' : '/',
   outputDir: 'dist/',
-  devServer,
+  // devServer,
   transpileDependencies: ['vuetify'],
   pluginOptions: {
     i18n: {
